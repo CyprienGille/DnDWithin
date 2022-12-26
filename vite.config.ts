e4070a8ab6ef9f5937bcc3e7becce3bc11cb8e98
CyprienGille <>
2022-12-26 17:05:15 +0100

    Attempt to use tauri 2.0.0a to run on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { internalIpV4 } from 'internal-ip'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
+export default defineConfig(async () => {
+  const host = await internalIpV4()
+
+  /** @type {import('vite').UserConfig} */
+  const config = {
+    plugins: [svelte()],
+    server: {
+      host: '0.0.0.0', // listen on all addresses
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        protocol: 'ws',
+        host,
+        port: 5183,
+      },
+    },
+  }
+
+  return config
 })
