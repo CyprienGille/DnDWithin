80f9d7b8f97886d733a0a5e4684baf526e515604
CyprienGille <cyprien.gille@orange.fr>
2024-09-23 17:28:50 +0200

    Create files for later

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod ability;
+mod character;
+mod commons;
+mod description;
+mod equipment;
+mod feature;
+mod spell;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
