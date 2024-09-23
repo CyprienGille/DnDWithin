10f166ea76dbc356e0a8c0e160bed97397fe0083
CyprienGille <cyprien.gille@orange.fr>
2024-09-23 17:28:32 +0200

    Start implementing abilities

---
+++ src-tauri/src/ability.rs
@@ -0,0 +1,29 @@
+pub enum Ability {
+    Strength,
+    Dexterity,
+    Constitution,
+    Intelligence,
+    Wisdom,
+    Charisma,
+}
+
+enum Skill {
+    Acrobatics,
+    AnimalHandling,
+    Arcana,
+    Athletics,
+    Deception,
+    History,
+    Insight,
+    Intimidation,
+    Investigation,
+    Medicine,
+    Nature,
+    Perception,
+    Performance,
+    Persuasion,
+    Religion,
+    SleightOfHand,
+    Stealth,
+    Survival,
+}
