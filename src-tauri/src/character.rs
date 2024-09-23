957dc4f78a693c1cb75a43bc5d3aa119a5d839ad
CyprienGille <cyprien.gille@orange.fr>
2024-09-24 00:22:49 +0200

    Start implementing character

--- src-tauri/src/character.rs
+++ src-tauri/src/character.rs
@@ -1,1 +1,34 @@
+use crate::{
+    ability::{Ability, Skill},
+    commons::Distance,
+    description::Background,
+    equipment::Item,
+    spell::Spell,
+};
 
+pub struct Character {
+    name: String,
+    class: String,
+    level: u32,
+    player_name: String,
+    species: String,
+    abilities: Vec<Ability>,
+    skills: Vec<Skill>,
+    background: Background,
+    senses: Vec<Sense>,
+    languages: Vec<String>,
+    equipment: Vec<Item>,
+    spell_list: Vec<Spell>,
+}
+
+pub struct Sense {
+    sense_type: SenseType,
+    range: Distance,
+}
+
+pub enum SenseType {
+    Blindsight,
+    Darkvision,
+    Tremorsense,
+    Truesight,
+}
