73feb537a77e16ee169a78c2132c39e0dd391622
CyprienGille <cyprien.gille@orange.fr>
2024-09-24 17:03:39 +0200

    Add Roll Component and temporary type

---
+++ src/lib/Roll.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { RollType } from './types';
+
+	export let roll: RollType;
+
+	function get_bonus_string(bonus: number): string {
+		if (bonus == 0) {
+			return '';
+		} else if (bonus > 0) {
+			return '+' + bonus;
+		}
+		return bonus.toString();
+	}
+</script>
+
+<main>
+	<span>{roll.dice_amount}{roll.dice}{get_bonus_string(roll.bonus)}</span>
+</main>
---
+++ src/lib/types.ts
@@ -0,0 +1,5 @@
+export class RollType {
+    dice:string="d20";
+    dice_amount:number=1;
+    bonus:number=0;
+}
