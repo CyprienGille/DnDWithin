e56e710b8985cf673ac57da8b7bd7be8341d18b6
CyprienGille <cyprien.gille@orange.fr>
2024-09-25 00:10:15 +0200

    Implement feature classification by origin

--- src-tauri/src/feature.rs
+++ src-tauri/src/feature.rs
@@ -4,6 +4,15 @@ pub struct Feature {
     name: String,
     reference: Option<Reference>,
     source: String,
+    given_by: FeatureOrigin,
     description: String,
     charges: Option<Charges>,
+}
+
+pub enum FeatureOrigin {
+    Species,
+    Class,
+    Feat,
+    Item,
+    Other,
 }
