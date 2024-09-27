cb6839c47c452ba80341d902c55f1598a3d7409a
CyprienGille <cyprien.gille@orange.fr>
2024-09-27 16:09:28 +0200

    Add character state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use character::CharacterState;
+
 mod ability;
 mod character;
 mod commons;
@@ -11,6 +13,7 @@ mod spell;
 
 fn main() {
     tauri::Builder::default()
+        .manage(CharacterState::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
