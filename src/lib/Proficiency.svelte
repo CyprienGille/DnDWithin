50591bbfcfd01144e8c52708342e5fc4d3839e1d
CyprienGille <cyprien.gille@orange.fr>
2024-09-29 22:48:47 +0200

    Start implementing proficiency selector

---
+++ src/lib/Proficiency.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let proficiency: string;
+</script>
+
+<select class="select" bind:value={proficiency}>
+	<option value="Not" />
+	<option value="Half" />
+	<option value="Proficient" />
+	<option value="Expert" />
+</select>
