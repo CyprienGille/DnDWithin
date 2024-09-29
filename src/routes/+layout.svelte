0357ee99893fae3357e21af8d5fcc9a3b30a8d49
CyprienGille <cyprien.gille@orange.fr>
2024-09-29 22:50:01 +0200

    Implement and style menu drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,26 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup, LightSwitch } from '@skeletonlabs/skeleton';
+	import { storePopup, LightSwitch, initializeStores, Drawer } from '@skeletonlabs/skeleton';
+	initializeStores();
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<!-- <LightSwitch /> -->
+<Drawer width="w-64">
+	<h1 class="h1 text-center mb-2">
+		<span
+			class="bg-gradient-to-br from-blue-900 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+			>DnDWithin</span
+		>
+	</h1>
+	<div class="container grid-cols-1 w-full text-center">
+		<button class="btn variant-soft-surface my-2">Open Character Sheet</button>
+		<!-- <button class="btn variant-soft-surface my-2">Close Character Sheet</button> -->
+		<button class="btn variant-soft-surface my-2">Export to JSON</button>
+		<div class="w-full justify-center flex my-2">
+			<LightSwitch width="w-16" height="h-8" />
+		</div>
+	</div>
+</Drawer>
 
 <slot />
