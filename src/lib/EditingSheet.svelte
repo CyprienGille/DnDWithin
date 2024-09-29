b547de3a676b6781675f924854a5f4a65461ee1d
CyprienGille <cyprien.gille@orange.fr>
2024-09-29 22:49:16 +0200

    Change header style

--- src/lib/EditingSheet.svelte
+++ src/lib/EditingSheet.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import Ability from './Ability.svelte';
+	import Abilities from './Abilities.svelte';
 
 	export let c;
 </script>
 
-<div class="h4">{c.name}</div>
-<div>{c.species}, {c.class} {c.level}</div>
-{#each c.abilities as ability}
-	<Ability {ability} />
-{/each}
+<div class="variant-ghost-surface rounded pl-2 pb-1">
+	<h3 class="h3">{c.name}</h3>
+	<div>{c.species}, {c.class}</div>
+</div>
+<Abilities abilities={c.abilities} st_fluff={c.st_fluff} />
