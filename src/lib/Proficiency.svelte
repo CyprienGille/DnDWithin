7b298b782e7c1eca3698875510648a7f557918bc
CyprienGille <cyprien.gille@orange.fr>
2024-09-30 02:34:43 +0200

    Implement proficiency button

--- src/lib/Proficiency.svelte
+++ src/lib/Proficiency.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+	import { mappingProf } from './commons';
+
 	export let proficiency: string;
+
+	let state = mappingProf.get(proficiency);
+
+	function cycleState() {
+		state = (state + 1) % 4;
+		proficiency = mappingProf.revGet(state);
+	}
 </script>
 
-<select class="select" bind:value={proficiency}>
-	<option value="Not" />
-	<option value="Half" />
-	<option value="Proficient" />
-	<option value="Expert" />
-</select>
+<div title={proficiency}>
+	{#if state == 0}
+		<button class="p-1" on:click={cycleState}>N</button>
+	{:else if state == 1}
+		<button class="p-1" on:click={cycleState}>H</button>
+	{:else if state == 2}
+		<button class="p-1" on:click={cycleState}>P</button>
+	{:else if state == 3}
+		<button class="p-1" on:click={cycleState}>E</button>
+	{/if}
+</div>
