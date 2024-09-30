3d41b1da40398433f604818f494c2232ff7b9e27
CyprienGille <cyprien.gille@orange.fr>
2024-09-30 02:33:28 +0200

    Better Dice ts equivalent and implement two way mapping class

--- src/lib/commons.ts
+++ src/lib/commons.ts
@@ -1,15 +1,57 @@
-export class RollType {
-    dice: string;
-    dice_amount: number;
-    bonus: number;
-    
-    constructor(dice:string, dice_amount:number, bonus:number) {
-        this.dice = dice;
-        this.dice_amount = dice_amount;
-        this.bonus = bonus;
+export class Dice {
+    [key: string]:number;
+
+    constructor(diceType:string, diceAmount:number) {
+        this[diceType] = diceAmount;
     }
 }
 
-export function add_sign(num: number): string {
+export function diceToString(dice:Dice):string {
+    let out = "";
+    for (const [key, value] of Object.entries(dice)) {
+        out = value + key;
+    }
+    return out;
+}
+
+export class RollType {
+    dice: Dice;
+    bonus: number;
+    reroll: string
+    
+    constructor(dice:Dice, bonus:number, reroll:string) {
+        this.dice = dice;
+        this.bonus = bonus;
+        this.reroll = reroll;
+    }
+}
+
+export function addSign(num: number): string {
     return (num >= 0 ? '+' : '') + num;
 }
+
+export class TwoWayStringToNum {
+    map: { [x: string]: number; };
+    reverseMap: { [x:number] : string};
+
+    constructor(map: { [x: string]: number; }) {
+       this.map = map;
+       this.reverseMap = {};
+
+       for(const key in map) {
+          const value = map[key];
+          this.reverseMap[value] = key;   
+       }
+    }
+
+    get(key: string):number { return this.map[key]; }
+    
+    revGet(key: number):string { return this.reverseMap[key]; }
+}
+
+export const mappingProf = new TwoWayStringToNum({
+    "NotProficient":0,
+    "HalfProficient":1,
+    "Proficient":2,
+    "Expert":3,
+})
