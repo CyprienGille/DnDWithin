6cb47622fcad8bc7f43aca64e5752e4567c3ab48
CyprienGille <cyprien.gille@orange.fr>
2024-10-19 22:52:51 +0200

    Add skills component

---
+++ src/lib/Skills.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { addSign } from './commons';
+	import Proficiency from './Proficiency.svelte';
+	import Roll from './Roll.svelte';
+
+	export let skills;
+</script>
+
+<main class="variant-ghost-surface rounded content-center align-middle">
+	{#each skills as skill}
+		<div class="flex flex-row w-fit text-sm">
+			<span class="w-4">
+				<Proficiency proficiency={skill.proficiency} />
+			</span>
+			<span class="px-2">
+				<Roll roll={skill.roll}>{addSign(skill.roll.bonus)}</Roll>
+			</span>
+			<span class="">
+				{skill.name}
+			</span>
+		</div>
+	{/each}
+</main>
