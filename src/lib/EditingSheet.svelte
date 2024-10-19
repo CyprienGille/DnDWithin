aa7208d2fd27f8a79856ef61f124abc4caa461d9
CyprienGille <cyprien.gille@orange.fr>
2024-10-19 22:53:04 +0200

    Include skills

--- src/lib/EditingSheet.svelte
+++ src/lib/EditingSheet.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Abilities from './Abilities.svelte';
+	import Skills from './Skills.svelte';
 
 	export let c;
 </script>
@@ -8,4 +9,7 @@
 	<h3 class="h3">{c.name}</h3>
 	<div>{c.species}, {c.class}</div>
 </div>
-<Abilities abilities={c.abilities} st_fluff={c.st_fluff} />
+<div class="flex flex-row">
+	<Abilities abilities={c.abilities} st_fluff={c.st_fluff} />
+	<Skills skills={c.skills} />
+</div>
